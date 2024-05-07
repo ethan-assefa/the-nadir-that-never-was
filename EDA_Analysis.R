2735dea495cd04b7d49e7992af53f4201330f6fd
ethan-assefa <affesa.ethan@gmail.com>
2024-05-06 21:50:48 -0400

    Needs more work for data

--- EDA_Analysis.R
+++ EDA_Analysis.R
@@ -19,3 +19,73 @@ reveal_dataset(Hist_US_Stat_Main)
 reveal_dataset(Hist_US_Stat_OccupDispar)
 
 
+Actual_HistStat <- Hist_US_Stat_Main %>%
+  select(Year, Race, SchoolEnroll_Percent, Illiteracy_Percent, Institutionalized_per10000)
+Actual_HistStat_black <- Actual_HistStat %>% 
+  filter(Race == "Black") %>% select(-Race)
+Actual_HistStat_black <- full_join(Actual_HistStat_black, B_Elect_Year, "Year")
+Actual_HistStat_white <- Actual_HistStat %>% 
+  filter(Race == "White") %>% select(-Race)
+
+
+
+
+impute_missing_values <- function(time_series_data, time_var, outcome_var) {
+  # Replace missing values with NA
+  time_series_data[is.na(time_series_data[[outcome_var]]), outcome_var] <- NA
+  
+  # Impute missing values using spline method
+  imputed_data <- na_interpolation(time_series_data[[outcome_var]], option = "spline")
+  
+  # Create a dataframe with time and imputed values
+  imputed_df <- data.frame(Time = time_series_data[[time_var]], Outcome = imputed_data)
+  
+  return(imputed_df)
+}
+
+impute_missing_values_multiple <- function(time_series_data, time_vars, outcome_vars) {
+  imputed_dfs <- map2(time_vars, outcome_vars, ~ {
+    time_var <- .x
+    outcome_var <- .y
+    
+    # Replace missing values with NA
+    time_series_data %>%
+      mutate(!!outcome_var := ifelse(is.na(!!sym(outcome_var)), NA, !!sym(outcome_var))) %>%
+      transmute(Time = !!sym(time_var), Outcome = na_interpolation(!!sym(outcome_var), option = "spline"))
+  })
+  
+  return(imputed_dfs)
+}
+
+# Example usage
+time_vars <- rep("Year", 3)
+outcome_vars <- c("SchoolEnroll_Percent", "Illiteracy_Percent", "Institutionalized_per10000")
+
+Actual_HistStat_black_imp <- impute_missing_values_multiple(Actual_HistStat_black, time_vars, outcome_vars)
+Actual_HistStat_white_imp <- impute_missing_values_multiple(Actual_HistStat_white, time_vars, outcome_vars)
+
+# BLACK DATA TO FEED MODEL
+SchoolEnroll_black_imp <- Actual_HistStat_black_imp[[1]]
+names(SchoolEnroll_black_imp) <- c("Year", "Enroll_Percent")
+Illiteracy_black_imp <- Actual_HistStat_black_imp[[2]]
+names(Illiteracy_black_imp) <- c("Year", "Illiteracy_Percent")
+Institutional_black_imp <- Actual_HistStat_black_imp[[3]]
+names(Institutional_black_imp) <- c("Year", "Institutionalized_per10000")
+Actual_HistStat_black_imp <- full_join(SchoolEnroll_black_imp, Illiteracy_black_imp, "Year")
+Actual_HistStat_black_imp <- full_join(Actual_HistStat_black_imp, Institutional_black_imp, "Year")
+Actual_HistStat_black_imp <- full_join(Actual_HistStat_black_imp, B_Elect_Year, "Year")
+
+# WHITE DATA TO FEED MODEL
+SchoolEnroll_white_imp <- Actual_HistStat_white_imp[[1]]
+names(SchoolEnroll_white_imp) <- c("Year", "Enroll_Percent")
+Illiteracy_white_imp <- Actual_HistStat_white_imp[[2]]
+names(Illiteracy_white_imp) <- c("Year", "Illiteracy_Percent")
+Institutional_white_imp <- Actual_HistStat_white_imp[[3]]
+names(Institutional_white_imp) <- c("Year", "Institutionalized_per10000")
+Actual_HistStat_white_imp <- full_join(SchoolEnroll_white_imp, Illiteracy_white_imp, "Year")
+Actual_HistStat_white_imp <- full_join(Actual_HistStat_white_imp, Institutional_white_imp, "Year")
+
+
+# Create csv files with clean, wrangled data
+write.csv(Actual_HistStat_black_imp, "Actual_HistStat_black_imp.csv")
+write.csv(Actual_HistStat_white_imp, "Actual_HistStat_white_imp.csv")
--- Modeling_Analysis_Main.R
+++ Modeling_Analysis_Main.R
@@ -9,6 +9,9 @@ library(naniar) # For seeing missingness
 library(tidycensus) # For getting built in state-county FIPS data 
 library(tseries) # For working with time series data
 library(forecast) # For creating forecasting models
+library(bsts) # For state-space model
+library(imputeTS)
+library(bsts)
 
 # Loads in the R environment containing the final versions of our clean data, as well as functions made
 load("AnalysisData_Clean.RData")
@@ -16,7 +19,7 @@ load("AnalysisData_Clean.RData")
 
 ############# Time Period ############# 
 # Determine time periods of interest era
-preCWreconstruction_start <- 1850 # Gives model idea of trends building into the civil war and then reconstruction
+preCWreconstruction_start <- 1810 # Gives model idea of trends building into the civil war and then reconstruction
 reconstruction_start <- 1863 # Strict start for reconstruction
 reconstruction_end <- 1877 # Strict end for reconstruction
 reconstruction_era_end <- 1890 # Looser definition of reconstruction "era"
@@ -103,16 +106,98 @@ Era_HistStat_OccDispr <- Hist_US_Stat_OccupDispar %>%
 
 
 ############# Examining Form of Data #############
+
 reveal_dataset(Era_B_Elect_State)
 reveal_dataset(Era_B_Elect_Year)
 reveal_dataset(Era_HistStat)
 reveal_dataset(Era_HistStat_OccDispr)
 
 
-############# Setting Up Time Series #############
+############# Create race specific datasets #############
 
-# Create time series object for 
-literacy_ts <- ts(literacy_ts$mean_literacy_rate, start = start_year, frequency = 1)
-# Decompose
-decomposed <- decompose(literacy_ts)
-plot(decomposed)
+Era_HistStat_black <- Era_HistStat %>% 
+  filter(Race == "Black")
+Era_HistStat_white <- Era_HistStat %>% 
+  filter(Race == "White")
+
+############# Imputing Missing Values #############
+
+impute_missing_values <- function(time_series_data, time_var, outcome_var) {
+  # Replace missing values with NA
+  time_series_data[is.na(time_series_data[[outcome_var]]), outcome_var] <- NA
+  
+  # Impute missing values using spline method
+  imputed_data <- na_interpolation(time_series_data[[outcome_var]], option = "spline")
+  
+  # Create a dataframe with time and imputed values
+  imputed_df <- data.frame(Time = time_series_data[[time_var]], Outcome = imputed_data)
+  
+  return(imputed_df)
+}
+
+impute_missing_values_multiple <- function(time_series_data, time_vars, outcome_vars) {
+  imputed_dfs <- map2(time_vars, outcome_vars, ~ {
+    time_var <- .x
+    outcome_var <- .y
+    
+    # Replace missing values with NA
+    time_series_data %>%
+      mutate(!!outcome_var := ifelse(is.na(!!sym(outcome_var)), NA, !!sym(outcome_var))) %>%
+      transmute(Time = !!sym(time_var), Outcome = na_interpolation(!!sym(outcome_var), option = "spline"))
+  })
+  
+  return(imputed_dfs)
+}
+
+# Example usage
+time_vars <- rep("Year", 3)
+outcome_vars <- c("SchoolEnroll_Percent", "Illiteracy_Percent", "Institutionalized_per10000")
+
+Era_HistStat_black_imp <- impute_missing_values_multiple(Era_HistStat_black, time_vars, outcome_vars)
+Era_HistStat_white_imp <- impute_missing_values_multiple(Era_HistStat_white, time_vars, outcome_vars)
+
+# BLACK DATA TO FEED MODEL
+SchoolEnroll_black_imp <- Era_HistStat_black_imp[[1]]
+names(SchoolEnroll_black_imp) <- c("Year", "Enroll_Percent")
+Illiteracy_black_imp <- Era_HistStat_black_imp[[2]]
+names(Illiteracy_black_imp) <- c("Year", "Illiteracy_Percent")
+Institutional_black_imp <- Era_HistStat_black_imp[[3]]
+names(Institutional_black_imp) <- c("Year", "Institutionalized_per10000")
+Era_HistStat_black_imp <- full_join(SchoolEnroll_black_imp, Illiteracy_black_imp, "Year")
+Era_HistStat_black_imp <- full_join(Era_HistStat_black_imp, Institutional_black_imp, "Year")
+# WHITE DATA TO FEED MODEL
+SchoolEnroll_white_imp <- Era_HistStat_white_imp[[1]]
+names(SchoolEnroll_white_imp) <- c("Year", "Enroll_Percent")
+Illiteracy_white_imp <- Era_HistStat_white_imp[[2]]
+names(Illiteracy_white_imp) <- c("Year", "Illiteracy_Percent")
+Institutional_white_imp <- Era_HistStat_white_imp[[3]]
+names(Institutional_white_imp) <- c("Year", "Institutionalized_per10000")
+Era_HistStat_white_imp <- full_join(SchoolEnroll_white_imp, Illiteracy_white_imp, "Year")
+Era_HistStat_white_imp <- full_join(Era_HistStat_white_imp, Institutional_white_imp, "Year")
+# ACTUAL DATA TO COMPARE
+Actual_HistStat <- Hist_US_Stat_Main %>% 
+  filter(Year > reconstruction_era_end) %>% 
+  select(Year, Race, SchoolEnroll_Percent, Illiteracy_Percent, Institutionalized_per10000)
+Actual_HistStat_black <- Actual_HistStat %>% 
+  filter(Race == "Black") %>% select(-Race)
+Actual_HistStat_white <- Actual_HistStat %>% 
+  filter(Race == "White") %>% select(-Race)
+
+
+
+############# Function for Forecasting Model #############
+
+ts_data <- ts(Era_HistStat_black_imp$Illiteracy_Percent, start = min(Era_HistStat_black_imp$Year), frequency = 10)
+# Create a time series object from the outcome data
+ts_data <- ts(Era_HistStat_black_imp$outcome, start = min(Era_HistStat_black_imp$year), frequency = 1/(max(Era_HistStat_black_imp$year) - min(Era_HistStat_black_imp$year)) * 10)
+
+# Bayesian structural time series
+fit_bsts <- function(data) {
+  ss_model <- AddLocalLinearTrend(list(), data)
+  ss_model <- bsts(data, state.specification = ss_model, niter = 1000)
+  predict(ss_model, horizon = 30, burn = 100)
+}
+
+# Example usage
+forecast_bsts <- fit_bsts(ts_data)
+plot(forecast_bsts)
